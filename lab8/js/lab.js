532f086ade4bffbf5cd7a82ce24d9645429f5d58
Adamsinclairswim <adamsinclairslc@gmail.com>
2025-05-08 18:45:41 -0700

    lab8 pt 2

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -5,25 +5,24 @@
  */
 
 
-// Create an array of numbers
+
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// Named function that squares a number
+
 function squareNumber(x) {
-    // Multiply the number by itself
+    
     var results = x * x;
     return results;
 }
 
-// Use map with the named function
 var squaredResults = numbers.map(squareNumber);
 
-// Anonymous function to add 10 to each number
+
 var addTenResults = numbers.map(function(x) {
     var results = x + 10;
     return results;
 });
 
-// Output the results to the webpage
+
 document.getElementById("output1").innerHTML = "Squared Results: " + squaredResults.join(", ");
 document.getElementById("output2").innerHTML = "Add Ten Results: " + addTenResults.join(", ");
