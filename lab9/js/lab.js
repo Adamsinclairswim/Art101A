7201cb4caacc18f85aedab66bedf4b047025ad8d
Adamsinclairswim <adamsinclairslc@gmail.com>
2025-05-12 21:06:27 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,24 +1,26 @@
 /*
    Requirements: jQuery must be loaded for this script to work.
-   Instructor:Wes modes
+   Instructor: Wes Modes
    Author: Adam Sinclair
    Date: 2025
 */
 
-// Add buttons to each section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-$("#results").append("<button id='button-results'>Make Special</button>");
+$(document).ready(function() {
+  // Add buttons to each section
+  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+  $("#problems").append("<button id='button-problems'>Make Special</button>");
+  $("#results").append("<button id='button-results'>Make Special</button>");
 
-// Add click event listeners
-$("#button-challenge").click(function(){
-  $("#challenge").toggleClass("special");
-});
+  // Add click event listeners
+  $("#button-challenge").click(function(){
+    $("#challenge").toggleClass("special");
+  });
 
-$("#button-problems").click(function(){
-  $("#problems").toggleClass("special");
-});
+  $("#button-problems").click(function(){
+    $("#problems").toggleClass("special");
+  });
 
-$("#button-results").click(function(){
-  $("#results").toggleClass("special");
+  $("#button-results").click(function(){
+    $("#results").toggleClass("special");
+  });
 });
