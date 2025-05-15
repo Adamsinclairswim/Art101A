df66f9a01be5d7f117b2c69500b1a5daa8827f80
Adamsinclairswim <adamsinclairslc@gmail.com>
2025-05-15 13:32:29 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,22 +1,36 @@
 /*
-   lab10.js - This simple JavaScript/jQuery script appends new elements to an output div
+   lab.js - This JavaScript/jQuery script appends random sentences to an output div
+
    Requirements: jQuery must be loaded for this script to work.
+
    Author: Adam Sinclair
    Date: 2025
 */
 
-// Helper function to generate random text
+// Array of real example sentences
+const sentences = [
+  "HI how are you doing?",
+  "I love learning JavaScript it's fun and powerful.",
+  "Santa Cruz is such a beautiful place to study and train.",
+  "Let's go for a bike ride this weekend!",
+  "Coding gets easier the more you practice it.",
+  "What do you think about AI and ethics?",
+  "I'm working on my ART101 lab right now!",
+  "Sometimes a little debugging goes a long way.",
+  "Don't forget to commit your changes to GitHub!",
+  "This lab is a great way to experiment with jQuery."
+];
+
+
+
+// Function to get a random sentence
 function generateRandomText() {
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const min = 3;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  return text.slice(randStart, randStart + randLen);
+  const index = Math.floor(Math.random() * sentences.length);
+  return sentences[index];
 }
 
-
-$("#make-convo").click(function() {
+// Click listener for button
+$("#make-convo").click(function () {
   const newText = generateRandomText();
   $("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
